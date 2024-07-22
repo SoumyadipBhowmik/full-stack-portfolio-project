dbfdedcd2e897281b253f31d3f486b74961e620c
Soumyadip Bhowmik <soumyadip.bhowmik@brewszilla.com>
2024-07-22 22:08:19 +0530

    UUID fixed here

--- src/main/java/com/jwt/auth/jwttutorial/repositories/OutletsRepository.java
+++ src/main/java/com/jwt/auth/jwttutorial/repositories/OutletsRepository.java
@@ -1,9 +1,11 @@
 package com.jwt.auth.jwttutorial.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jwt.auth.jwttutorial.models.Outlets;
 
-public interface OutletsRepository extends JpaRepository<Outlets, String>{
+public interface OutletsRepository extends JpaRepository<Outlets, UUID>{
 
 }
